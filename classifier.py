63852d2a0414c40991f8c9576a1537395f455c30
ziwei wu <ziweiwu@gmail.com>
2019-02-25 15:43:10 -0500

    reformatted classifier.py

--- classifier.py
+++ classifier.py
@@ -3,7 +3,6 @@
 import itertools
 import pandas as pd
 import json
-import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -44,16 +43,20 @@ def is_top_alexa(s):
 
 def model_tune_params(model, params, X, Y):
     'tune a machine learning model'
-    new_model = GridSearchCV(estimator=model,
-                             param_grid=params, cv=5, n_jobs=-1,
-                             )
+    new_model = GridSearchCV(
+        estimator=model,
+        param_grid=params,
+        cv=5,
+        n_jobs=-1,
+    )
     new_model.fit(X, Y)
     print(new_model, '\n')
     return new_model
 
 
 # plot confusion matrix
-def plot_confusion_matrix(cm, classes,
+def plot_confusion_matrix(cm,
+                          classes,
                           normalize=False,
                           title='Confusion matrix',
                           cmap=plt.cm.Blues):
@@ -79,9 +82,12 @@ def plot_confusion_matrix(cm, classes,
     fmt = '.2f' if normalize else 'd'
     thresh = cm.max() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
+        plt.text(
+            j,
+            i,
+            format(cm[i, j], fmt),
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
 
     plt.tight_layout()
     plt.ylabel('True label')
@@ -91,57 +97,38 @@ def plot_confusion_matrix(cm, classes,
 def main():
     corpus = open('data/train.json')
     urldata = json.load(corpus, encoding="latin1")
-    X_table = [];
-    X_header = ["url", "host_length", "url_length", "domain_age", "domain_token_count",
-                "path_length", "path_token_count", "is_top_alexa", "alexa_rank_level"
-                ];
+    X_table = []
+    X_header = [
+        "url", "host_length", "url_length", "domain_age", "domain_token_count",
+        "path_length", "path_token_count", "is_top_alexa", "alexa_rank_level"
+    ]
     Y_header = ["malicious"]
-    Y = [];
+    Y = []
 
     for row in urldata:
-        data_row = [];
-        print ''
-        print 'url: %s' % row["url"]
+        data_row = []
         data_row.append(row["url"])
-
-        print 'host length: %d' % int(row["host_len"])
         data_row.append(int(row["host_len"]))
-
-        print 'url length: %d' % int(row["url_len"])
         data_row.append(int(row["url_len"]))
-
-        print 'domain age: %d' % int(row["domain_age_days"])
         data_row.append(int(row["domain_age_days"]))
-
-        print 'domain token count: %d' % int(row["num_domain_tokens"])
         data_row.append(int(row["num_domain_tokens"]))
-
-        print 'path length: %d' % int(row["path_len"])
         data_row.append(int(row["path_len"]))
-
-        print 'path token count: %d' % int(row["num_path_tokens"])
         data_row.append(int(row["num_path_tokens"]))
-
-        print 'Top Alexa: %d' % is_top_alexa(row["alexa_rank"])
         data_row.append(is_top_alexa(row["alexa_rank"]))
-
-        print 'Alexa rank level: %d' % get_alexa_rank_level(row["alexa_rank"])
         data_row.append(get_alexa_rank_level(row["alexa_rank"]))
 
         X_table.append(data_row)
-
-        print 'malicious: %d' % row["malicious_url"]
         Y.append(row["malicious_url"])
 
     # create a panda dataframe
-    X = pd.DataFrame(X_table, columns=X_header);
+    X = pd.DataFrame(X_table, columns=X_header)
     Y = pd.DataFrame(Y, columns=Y_header)
-    print X.head();
-    print Y.head();
+    print X.head()
+    print Y.head()
 
     # drop url for now
     X_new = X.drop(['url'], axis=1)
-    print X_new.head();
+    print X_new.head()
 
     # split dataset into training and testing
     X_train, X_test, Y_train, Y_test \
@@ -178,7 +165,8 @@ def main():
 
     logit = model_tune_params(logit, logit_params, X_train, Y_train)
     svm1 = model_tune_params(linear_svm, linear_svm_params, X_train, Y_train)
-    svm2 = model_tune_params(none_linear_svm, none_linear_svm_params, X_train, Y_train)
+    svm2 = model_tune_params(none_linear_svm, none_linear_svm_params, X_train,
+                             Y_train)
     rf = model_tune_params(rf, rf_params, X_train, Y_train)
 
     logit_pred = logit.predict(X_test)
@@ -203,7 +191,8 @@ def main():
     logit_matrix = confusion_matrix(Y_test, logit_pred)
     np.set_printoptions(precision=2)
     plt.figure()
-    plot_confusion_matrix(logit_matrix, classes, normalize=False, title='logit')
+    plot_confusion_matrix(
+        logit_matrix, classes, normalize=False, title='logit')
     plt.savefig('images/logit.png')
 
     svm1_matrix = confusion_matrix(Y_test, svm1_pred)
@@ -225,8 +214,6 @@ def main():
     plt.savefig('images/rf.png')
 
     # make prediction on classify.json
-    svm1 = model_tune_params(linear_svm, linear_svm_params, X_train, Y_train)
-
     classify_json = open('data/classify.json')
     classify_data = json.load(classify_json, encoding="latin1")
 
@@ -235,7 +222,7 @@ def main():
     classify_urls = []
     for row in classify_data:
         classify_urls.append(row["url"])
-        data_row = [];
+        data_row = []
         data_row.append(int(row["host_len"]))
         data_row.append(int(row["url_len"]))
         data_row.append(int(row["domain_age_days"]))
@@ -249,7 +236,7 @@ def main():
     classify_X_df = pd.DataFrame(classify_X)
     classify_pred = svm1.predict(classify_X_df)
 
-    # output the predict results of classify.json
+    # output the predict results to results.txt
     results = open("results.txt", "w+")
     for i in range(0, len(classify_pred)):
         print "%s, %d" % (classify_urls[i], classify_pred[i])
